33dfae1e2721db22c23968c313e6175482261b42
손진우 <sonjinwoo0729@gmail.com>
2024-09-24 15:58:09 +0900

    feat:make serializers, models, views
    serializers, models 일부 완성, views 메소드별 정리*

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .serializers import UserSerializer
 # Create your views here.
+
+
+class UserCreateAPIView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+class UserUpdateAPIView(generics.UpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
+
