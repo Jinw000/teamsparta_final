53ee67bb03b90484a5f9f6133b6bc560f8506cee
chanloper <dbstjddl0396@naver.com>
2024-09-25 16:31:40 +0900

    Like,DisLike

--- users/models.py
+++ users/models.py
@@ -1,6 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
+
 class user(models.Model):
     pass
+
+
+class Like(models.Model):
+    user_profile = models.ForeignKey(User, on_delete=models.CASCADE)
+    liked_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user_profile', 'liked_by')
+
+
+class Dislike(models.Model):
+    user_profile = models.ForeignKey(User, on_delete=models.CASCADE)
+    disliked_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user_profile', 'disliked_by')
--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from . import views
+from .views import ProfileView, LikeView, DislikeView
 
 urlpatterns = [
-    path("<str:username>/", views.ProfileView.as_view()),
+    path("<str:username>/", ProfileView.as_view()),
+    path('<int:user_profile_id>/like/', LikeView.as_view(), name='like-profile'),
+    path('<int:user_profile_id>/dislike/',
+         DislikeView.as_view(), name='dislike-profile'),
 ]
